88d5e57a6da6181444aacb9f053b2a3792cfa31a
kamal <kamal@gmail.com>
2018-01-17 23:15:21 +0530

    km

--- app/src/main/java/com/demo/MainActivity.java
+++ app/src/main/java/com/demo/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends BaseActivity
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
